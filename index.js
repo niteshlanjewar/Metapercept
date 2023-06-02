528a9a9e93451354af5bacdd3fadb649765884b9
niteshlanjewar <nitlanjewar1621@gmail.com>
2023-06-02 17:25:04 +0530

    site

---
+++ index.js
@@ -0,0 +1,18 @@
+fetch('./class.json').then((response.json)).then((json.card)=>)
+console.log(jsondata)
+const cardsContainer= document.getElementById("card-container");
+
+jsonData.forEach((item)) => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const title = document.createElement("h2");
+    title.textContent = item.title;
+
+    const description = document.createElement("p");
+    description.textContent = item.description;
+
+    card.appendChild(title);
+    card.appendChild(description);
+    card.appendChild(card);
+});
